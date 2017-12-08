851b93d86bc136615944a0d502d7c2b95a25f58d
Remzo <remystuurwold@gmail.com>
2017-12-08 21:46:25 +0100

    init

---
+++ SimpleWebsocketClient/Assets/Scripts/CentralServer/CentralServerConnection.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using WebSocketSharp;
+
+/// <summary>
+/// Convenience wrapper to handle and register callbacks from SocketIOComponent
+/// </summary>
+[RequireComponent(typeof(SocketIOComponent))]
+public class CentralServerConnection : MonoBehaviour {
+
+    public delegate void ConnectionCallback();
+    public delegate void ConnectionCallbackString(string data);
+
+    public ConnectionCallback OnConnectCallback;
+    public ConnectionCallback OnDisconnectCallback;
+    public ConnectionCallback OnErrorCallback;
+
+    private SocketIOComponent m_Socket = null;
+    private Dictionary<string, Action<string>> m_RegisteredCallbacks = new Dictionary<string, Action<string>>();
+
+    private void Awake()
+    {
+        m_Socket = this.GetComponent<SocketIOComponent>();
+
+        //register handlers
+        m_Socket.On("connect", OnConnect);
+        m_Socket.On("disconnect", OnDisconnect);
+        m_Socket.On("error", OnError);
+        m_Socket.On("ConnectionID", OnConnectionID);
+    }
+
+    private void OnEnable()
+    {
+        m_Socket.OnHandlePacket += OnHandlePacket;
+    }
+
+    private void OnDisable()
+    {
+        m_Socket.OnHandlePacket -= OnHandlePacket;
+    }
+
+    public void Connect()
+    {
+        m_Socket.Connect();
+    }
+
+    public void Disconnect()
+    {
+        m_Socket.Close();
+    }
+
+    public void RegisterCustomCallback(string key, Action<string> callback)
+    {
+        if (!m_RegisteredCallbacks.ContainsKey(key))
+        {
+            m_RegisteredCallbacks.Add(key, callback);
+        }
+        else
+        {
+            Debug.LogWarning("Callback already registered for key: " + key + ". Overwriting..");
+            m_RegisteredCallbacks[key] = callback;
+        }
+    }
+
+    public void DeregisterCustomCallback(string key)
+    {
+        if (!m_RegisteredCallbacks.ContainsKey(key))
+        {
+            Debug.LogWarning("No callback registered for key: " + key);
+        }
+        else
+        {
+            m_RegisteredCallbacks.Remove(key);
+        }
+    }
+
+    public void SendToServer(string key, string msg = null)
+    {
+        if (msg != null)
+        {
+            var msgJSON = JSONObject.StringObject(msg); //send all socket data as a json object
+            m_Socket.Emit(key, msgJSON);
+        }
+        else
+        {
+            m_Socket.Emit(key);
+        }
+    }
+
+    private void OnConnect(SocketIO.SocketIOEvent e)
+    {
+        Debug.Log("Connection established");
+        if (OnConnectCallback != null)
+        {
+            OnConnectCallback();
+        }
+    }
+
+    private void OnDisconnect(SocketIO.SocketIOEvent e)
+    {
+        Debug.Log("Connection closed");
+        if (OnDisconnectCallback != null)
+        {
+            OnDisconnectCallback();
+        }
+    }
+
+    private void OnError(SocketIO.SocketIOEvent e)
+    {
+        Debug.LogWarning("WebSocket error.");
+        if (OnErrorCallback != null)
+        {
+            OnErrorCallback();
+        }
+    }
+
+    private void OnConnectionID(SocketIOEvent e)
+    {
+        Debug.Log("connectionid: " + e.data[0].ToString());
+    }
+
+    private void OnHandlePacket(Packet packet)
+    {
+        // get msg type
+        var msgType = RemoveFirstAndLastChar(packet.json.list[0].ToString()); // get msg key and remove quotations
+        Debug.Log("Packet message type: " + msgType);
+
+        var stringData = RemoveFirstAndLastChar(packet.json.list[1].ToString());
+
+        if (m_RegisteredCallbacks.ContainsKey(msgType))
+        {
+            if (m_RegisteredCallbacks[msgType] != null)
+            {
+                m_RegisteredCallbacks[msgType](stringData);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No callback registered for key: " + msgType);
+        }
+
+        //if (msgType == "ConnectionID")
+        //{
+        //    var rawMsg = packet.json.list[1].ToString();
+        //    var connectionID = rawMsg.Substring(1, rawMsg.Length - 2);
+
+        //    Debug.Log("connectionID: " + connectionID);
+        //}
+    }
+
+    private void Update()
+    {
+    }
+
+    private string RemoveFirstAndLastChar(string str)
+    {
+        if (str.Length >= 4)
+        {
+            return str.Substring(1, str.Length - 2);
+        }
+        else
+        {
+            Debug.LogWarning("Invalid str. Too short.");
+            return str;
+        }
+    }
+}
---
+++ SimpleWebsocketClient/Assets/Scripts/Controller.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    [SerializeField]
+    private CentralServerConnection m_CentralServer = null;
+
+    [Header("UI Common")]
+    [SerializeField]
+    private GameObject m_Prompt = null;
+    [SerializeField]
+    private GameObject m_Common = null;
+    [SerializeField]
+    private Text m_MessageBox = null;
+    [SerializeField]
+    private Text m_DataBox = null;
+    [Header("UI Sender")]
+    [SerializeField]
+    private GameObject m_Sender = null;
+    [SerializeField]
+    private Text m_SenderMessageBox = null;
+    [SerializeField]
+    private Text m_SenderConnectionID = null;
+    [Header("UI Receiver")]
+    [SerializeField]
+    private GameObject m_Receiver = null;
+    [SerializeField]
+    private Text m_ReceiverMessageBox = null;
+    [SerializeField]
+    private GameObject m_GetDataButton = null;
+
+    private bool b_CallbacksRegistered = false;
+
+    private bool b_Connected = false;
+    private bool b_IsSender = false;
+
+    private bool b_ConnectionIDReceived = false;
+    private bool b_ItemDataRecieved = false;
+
+    private string m_ItemID = "";
+    private string m_ConnectionID = "";
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    private void OnEnable()
+    {
+        if (!b_CallbacksRegistered)
+        {
+            RegisterCallbacks();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (b_CallbacksRegistered)
+        {
+            DeregisterCallbacks();
+        }
+        StopAllCoroutines();
+    }
+
+    private void RegisterCallbacks()
+    {
+        b_CallbacksRegistered = true;
+        m_CentralServer.OnConnectCallback += OnConnected;
+        m_CentralServer.OnDisconnectCallback += OnDisconnect;
+        m_CentralServer.OnErrorCallback += OnError;
+
+        m_CentralServer.RegisterCustomCallback("ConnectionID", OnConnectionIDReceived);
+        m_CentralServer.RegisterCustomCallback("ItemID", OnItemIDReceived);
+        m_CentralServer.RegisterCustomCallback("ItemReceived", OnItemReceived);
+    }
+
+    private void DeregisterCallbacks()
+    {
+        b_CallbacksRegistered = false;
+        m_CentralServer.OnConnectCallback -= OnConnected;
+        m_CentralServer.OnDisconnectCallback -= OnDisconnect;
+        m_CentralServer.OnErrorCallback -= OnError;
+
+        m_CentralServer.DeregisterCustomCallback("ConnectionID");
+        m_CentralServer.DeregisterCustomCallback("ItemID");
+        m_CentralServer.DeregisterCustomCallback("ItemReceived");
+    }
+
+    private void OnConnected()
+    {
+        //handle state on connect
+        b_Connected = true;
+        if (b_IsSender)
+        {
+            m_CentralServer.SendToServer("Host", m_ItemID); //send host message and itemID to server to receive connection ID
+            StartCoroutine(WaitForConnectionID()); //start coroutine to wait for connection id
+            StartCoroutine(WaitForItemReceived());//start coroutine to wait for item received
+        }
+        else
+        {
+            m_CentralServer.SendToServer("GetItem", m_ConnectionID);
+            StartCoroutine(WaitForItemID());
+        }
+    }
+
+    private void OnDisconnect()
+    {
+        //handle state on disconnect
+        Debug.Log("Disconnected");
+    }
+
+    private void OnError()
+    {
+        //handle state on error
+    }
+
+    private void OnConnectionIDReceived(string connectionID)
+    {
+        Debug.Log("ConnectionID: " + connectionID);
+
+        m_ConnectionID = connectionID; //set connection ID for display
+        b_ConnectionIDReceived = true; //flag received for coroutine
+    }
+
+    private IEnumerator WaitForConnectionID()
+    {
+        while (!b_ConnectionIDReceived)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        //display connection id
+        m_SenderConnectionID.text = m_ConnectionID;
+
+        //reset flag
+        b_ConnectionIDReceived = false;
+
+        yield return null;
+    }
+
+    private void OnItemReceived(string msg)
+    {
+        //item received by reciever
+        b_ItemDataRecieved = true;
+
+        Debug.Log("Item received by receiver.");
+    }
+
+    private IEnumerator WaitForItemReceived()
+    {
+        while (!b_ItemDataRecieved)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        //close connection
+        Disconnect();
+
+        b_ItemDataRecieved = false;
+    }
+
+    public void SetConnectionID(string connID)
+    {
+        m_ConnectionID = connID.ToUpper();
+    }
+
+    public void GetItemFromConnectionID()
+    {
+        if (!IsOnline())
+        {
+            m_DataBox.text = "Please connect to the internet.";
+            return;
+        }
+
+        if (m_ConnectionID == "") //connection ID validation goes here
+        {
+            Debug.LogWarning("Input valid connection id");
+            return;
+        }
+
+        m_CentralServer.Connect();
+        m_GetDataButton.SetActive(false);
+    }
+
+    private void OnItemIDReceived(string itemID)
+    {
+        Debug.Log("ItemID: " + itemID);
+
+        m_ItemID = itemID; //set item ID for display
+        b_ItemDataRecieved = true; //flag received for coroutine
+    }
+
+    private IEnumerator WaitForItemID()
+    {
+        while (!b_ItemDataRecieved)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        b_ItemDataRecieved = false;
+
+        if (m_ItemID == "INVALID") //item id validation here
+        {
+            m_DataBox.text = "Invalid connection ID";
+            m_GetDataButton.SetActive(true);
+        }
+        else
+        {
+            m_DataBox.text = "Data: " + m_ItemID;
+            m_CentralServer.SendToServer("ItemReceived", m_ConnectionID);
+        }
+
+        Disconnect();//disconnect from central server
+    }
+
+    public void SetItemUpForGrabs(string itemID)
+    {
+        m_ItemID = itemID;
+    }
+
+    public void PutItemUpForGrabs()
+    {
+        if (!IsOnline())
+        {
+            m_MessageBox.text = "Please connect to the internet.";
+            return;
+        }
+
+        if (m_ItemID == "")
+        {
+            m_MessageBox.text = "Enter an itemID to send";
+            return;
+        }
+        b_IsSender = true;
+        m_CentralServer.Connect();
+
+        //init UI
+        m_Prompt.SetActive(false);
+        m_Sender.SetActive(true);
+        m_Common.SetActive(true);
+        m_Receiver.SetActive(false);
+        m_SenderMessageBox.text = "";
+        m_DataBox.text = "Data: " + m_ItemID;
+        m_SenderConnectionID.text = "";
+    }
+
+    public void InitReceiver()
+    {
+        b_IsSender = false;
+        m_Prompt.SetActive(false);
+        m_Sender.SetActive(false);
+        m_Receiver.SetActive(true);
+        m_Common.SetActive(true);
+        m_GetDataButton.SetActive(true);
+    }
+
+    public void BackToPrompt()
+    {
+        Disconnect();
+        StopAllCoroutines();
+        m_Prompt.SetActive(true);
+        m_Sender.SetActive(false);
+        m_Common.SetActive(false);
+        m_Receiver.SetActive(false);
+        m_MessageBox.text = "";
+        m_DataBox.text = "";
+    }
+
+    private bool IsOnline()
+    {
+        return Application.internetReachability != NetworkReachability.NotReachable;
+    }
+
+    private void Disconnect()
+    {
+        if (b_Connected)
+        {
+            b_Connected = false;
+            StopAllCoroutines();
+            m_CentralServer.Disconnect();
+        }
+    }
+}
